b283ff96c92b176542c4e29c3cb6bb942e603671
Tony <124948941+Club-Tony@users.noreply.github.com>
2023-11-17 02:15:07 -0800

    DS style pygame script, very barebones

--- CS50 final proj/game.py
+++ CS50 final proj/game.py
@@ -1,25 +1,44 @@
-import sys, pygame
+import pygame
+import sys
+
+# Initialize Pygame
 pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
+# Constants
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
-screen = pygame.display.set_mode(size)
+# Player
+player_size = 50
+player_x = WIDTH // 2 - player_size // 2
+player_y = HEIGHT - 2 * player_size
+player_speed = 3
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+# Create Pygame screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Dark Souls-like Side Scroller")
+clock = pygame.time.Clock()
 
+# Game loop
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and player_x > 0:
+        player_x -= player_speed
+    if keys[pygame.K_RIGHT] and player_x < WIDTH - player_size:
+        player_x += player_speed
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
+    # Update game entities, handle collisions, etc.
+
+    # Draw everything
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, WHITE, [player_x, player_y, player_size, player_size])
+
     pygame.display.flip()
+    clock.tick(FPS)
