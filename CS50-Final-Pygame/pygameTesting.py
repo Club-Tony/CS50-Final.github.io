d5ba9fdaf1e3e8494d75e2979ae43e70aeb365e8
Tony <124948941+Club-Tony@users.noreply.github.com>
2023-11-30 23:19:32 -0800

    save state

--- CS50-Final-Pygame/pygameTesting.py
+++ CS50-Final-Pygame/pygameTesting.py
@@ -30,18 +30,20 @@ not_fullscreen = False
 # define game variables(scrolling)
 scroll = 0
 
-# load ground background layers so they can be set manually at different speeds (7.png, 8.png)
+# load backgrounds starting with ground:
+# note: loading all 1 by 1 instead of list so I have full control over individual .png scrolling speed
 ground_image = pygame.image.load("C:\\Users\\Davey\\Documents\\GitHub\\CS50-Final.github.io\\CS50-Final-Pygame\\Island\\Layers\\L5.png").convert_alpha()
 
 # get ground background width and height
 ground_width = ground_image.get_width()
 ground_height = ground_image.get_height()
 
-# load water layers
+# load water with mountains
 water_mountains = pygame.image.load("C:\\Users\\Davey\\Documents\\GitHub\\CS50-Final.github.io\\CS50-Final-Pygame\\Island\\Layers\\L3.png").convert_alpha()
 water_mountains_width = water_mountains.get_width()
 water_mountains_height = water_mountains.get_height()
 
+# load water with trees
 water_trees = pygame.image.load("C:\\Users\\Davey\\Documents\\GitHub\\CS50-Final.github.io\\CS50-Final-Pygame\\Island\\Layers\\L4.png").convert_alpha()
 water_trees_width = water_trees.get_width()
 water_trees_height = water_trees.get_height()
@@ -60,41 +62,14 @@ far_mountains_height = far_mountains.get_height()
 sun = pygame.image.load("C:\\Users\\Davey\\Documents\\GitHub\\CS50-Final.github.io\\CS50-Final-Pygame\\Island\\Layers\\L6.png").convert_alpha()
 sun_width = sun.get_width()
 sun_height = sun.get_height()
-
-# load in background images, create list, loop, load image layers
-# blank list
-backgrounds = []
-
-# loop, make range 1-3 since I have 2 bg image layers that need to be loaded, 1 ground image for later (L5)
-# and 2 other layers that need to be at their own speed 
-# f-string to make it so {i} replaced with the value of i in each iteration of loop, note the subdirectories
-# and include L before {i} because each of my .png starts with an L before the number i.e. (L1.png)
-# convert_alpha to convert image and maintain transparency
-for i in range(1, 2):
-    background = pygame.image.load(f"Island/Layers/L{i}.png").convert_alpha()
-    
-    # append to add the loaded images into my list (backgrounds)
-    backgrounds.append(background)
-    
-# determine background image width (will allow for srolling later)
-background_width = backgrounds[0].get_width()
-    
-# draw "background" images (far mountains, and clouds) onto screen, iterate through list
-def draw_background():
-    # x variable loop so images draw next to eachother
-    # note: range is number of each layer being loaded, so in this case getting to 25 can be the end of game
-    for x in range(101):
-        # set different speeds of background layers to create parallax effect,
-        # scroll * speed so scroll variable will be adjusted based on speed that each image is set to
-        speed = 0.5
-        # note: the furthest image (far mountains) is being drawn first, closest image (clouds) drawn last
-        for i in backgrounds:
-            screen.blit(i, ((x * background_width) - scroll * speed, 0))
-            # increase speed at each iteration, this will make clouds move 0.3 faster than far mountains
-            speed += 0.3
             
-# draw ground images onto screen, they will be moving at a different speed so doing separately
-# range of 101 since game stops scrolling further right at 100 range (192,000 pixels)
+# draw far mountains
+# range of 101 for these since I'll stop (right) parallax scrolling at 100 range (192,000 pixels)
+def draw_far_mountains():
+    for x in range(101):
+        screen.blit(far_mountains, ((x * far_mountains_width) - scroll * 0.4, 0))
+            
+# draw ground images
 def draw_ground():
     for x in range(101):
         screen.blit(ground_image, ((x * ground_width) - scroll * 6, 0))
@@ -111,7 +86,7 @@ def draw_water_trees():
 # draw clouds and sun (edited these to scale/make transparent through gimp)
 def draw_clouds():
     for x in range (101):
-        screen.blit(clouds, ((x * clouds_width) - scroll * 0.3, 0))
+        screen.blit(clouds, ((x * clouds_width) - scroll * 0.2, 0))
 
 def draw_sun():
     for x in range (101):
@@ -119,6 +94,14 @@ def draw_sun():
         
 # make list of loaded images to simplify transform.scale
 images_list = [far_mountains, clouds, water_mountains, water_trees, ground_image, sun]
+
+# resize images with screen toggle: blank list, the for loop for transform/scale/append
+def resize_images(images, screen_width, screen_height):
+    resized_images = []
+    for image in images:
+        resized_image = pygame.transform.scale(image, (screen_width, screen_height))
+        resized_images.append(resized_image)
+    return resized_images
 
 # game loop, with forever repeating while loop (when run = true)
 run = True
@@ -136,7 +119,7 @@ while run:
     screen.fill((0, 0, 0))
     
     # draw background(far mountains), clouds, water/tree/close mountains layers, and ground in game loop
-    draw_background()
+    draw_far_mountains()
     draw_sun()
     draw_clouds()
     draw_water_mountains()
@@ -183,13 +166,16 @@ while run:
                 else:
                     screen_width = 1920
                     screen_height = 1080
-                       
-                # resize images with screen toggle: blank list, the for loop for load/transform/scale/append
-                resized_images = []
-                for image in images_list:
-                    resized_image = pygame.transform.scale(image, (screen_width, screen_height))
-                    resized_images.append(resized_image)
-                
+
+                # draw background layers again if fullscreen button toggled
+                resized_images = resize_images(images_list, screen_width, screen_height)
+                draw_far_mountains()
+                draw_sun()
+                draw_clouds()
+                draw_water_mountains()
+                draw_water_trees()
+                draw_ground()
+
                 # set display mode 
                 pygame.display.set_mode((screen_width, screen_height))
                     
